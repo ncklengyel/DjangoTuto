cae193ee68d9192e4e3a1422ff2bf9ca0b7bf33e
Nick Lengyel <Nick@Nicks-Air.local>
2017-03-20 16:50:27 -0400

    Tuto 22

---
+++ accounts/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'password1',
+        'password2',
+        ]
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name'] # contre Injection SQL
+        user.last_name = self.cleaned_data['last_name'] # contre Injection SQL
+        email = self.cleaned_data['email'] # contre Injection SQL
+
+        if commit:
+            user.save()
+
+        return user
+
+'''
+form qui herite de UserChangeForm pour edit les info du user
+'''
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = [
+        'email',
+        'first_name',
+        'last_name',
+        'password',
+        ]
---
+++ accounts/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from accounts.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+
+
+def home(request):
+    numbers = [1,2,3,4,5]
+    name = 'nick'
+    args = {'name': name, 'numbers': numbers}
+    return render(request, 'accounts/home.html', args)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/account/success/')
+
+        #SI le form n'est pas bon
+        else:
+            return redirect('/account/register/')
+
+    else:
+        form = RegistrationForm()
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
+
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
+
+#view pour la page de changement d'info du user
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile/')
+
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
+
+def success(request):
+    return render(request, 'accounts/success.html')
+
+#View pour le changement de password d'un user
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user) #garde la session active mm quand on change le password
+            return redirect('/account/profile/')
+
+        else:
+            return redirect('/account/change-password/')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
