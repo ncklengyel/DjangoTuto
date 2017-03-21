8f89c8ca833a7b790a88f087233dcf834cd88a5f
Nick Lengyel <Nick@Nicks-Air.local>
2017-03-20 22:32:15 -0400

    tuto24 navbar for logged users and decorator login_required

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'nick'
@@ -29,11 +30,13 @@ def register(request):
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
 
+@login_required #decorateur pour que seulement les loged in users peuvent voir la page
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
 
 #view pour la page de changement d'info du user
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -46,10 +49,12 @@ def edit_profile(request):
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
 
+#page pour account successful created
 def success(request):
     return render(request, 'accounts/success.html')
 
 #View pour le changement de password d'un user
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
